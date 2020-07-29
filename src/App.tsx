acb80023b2632c457ae317531ed803275e928c88
rifandani <ipandani2505@gmail.com>
2020-07-29 22:56:42 +0800

    add devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
-
+import { ReactQueryDevtools } from 'react-query-devtools';
+// components
 import Navbar from './components/Navbar';
 import People from './components/People';
 import Planets from './components/Planets';
 
 const App: React.FC = () => {
-  const [page, setPage] = useState('planets')
+  const [page, setPage] = useState('planets');
 
   return (
-    <div className="App">
-      <h1>Starwars Info</h1>
-      <Navbar setPage={setPage} />
+    <>
+      <div className="App">
+        <h1>Starwars Info</h1>
+        <Navbar setPage={setPage} />
 
-      <div className="content">
-        {page === 'planets' ? <Planets /> : <People />}
+        <div className="content">
+          {page === 'planets' ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { useQuery } from 'react-query'
-import Planet from './Planet'
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+// components
+import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('http://swapi.dev/api/planets/')
+const fetchPlanets = async (key: string, page: number) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
 
   // return Promise
-  return res.json()
-}
+  return res.json();
+};
 
 const Planets = () => {
+  const [page, setPage] = useState(1);
+
   /*
   status = 'loading' | 'success' | 'error'
   data = {
@@ -18,28 +21,35 @@ const Planets = () => {
     previous: null;
     results: array;
   }
+  array[0] = queryKey / key
+  sisa arraynya untuk query params, penamaan terserah
   */
-  const {data, status} = useQuery('planets', fetchPlanets)
-  console.log(data)
+  const { data, status } = useQuery(['planets', page], fetchPlanets, {
+    // staleTime: 1000, // default 0
+    // cacheTime: 10, // default 300000
+    onSuccess: () =>
+      console.log('Data fetch success, add notif onSuccess fetch'),
+  });
 
   return (
     <div>
       <h2>Planets</h2>
 
-      {status === 'loading' && (
-        <div>Loading Data</div>
-      )}
-      {status === 'error' && (
-        <div>Error Fetching Data</div>
-      )}
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
+
+      {status === 'loading' && <div>Loading Data</div>}
+      {status === 'error' && <div>Error Fetching Data</div>}
       {status === 'success' && (
         <div>
-          { data?.results.map((planet: any, i: number) => <Planet key={i} planet={planet} />) }
+          {data?.results.map((planet: any, i: number) => (
+            <Planet key={i} planet={planet} />
+          ))}
         </div>
       )}
-
     </div>
-  )
-}
+  );
+};
 
-export default Planets
+export default Planets;
